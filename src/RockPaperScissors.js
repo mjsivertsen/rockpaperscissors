598562a547894116f12c913b2e9de07090c38a4e
Michaela Sivertsen <mjsivertsen@msn.com>
2021-09-21 13:35:40 -0600

    working better than before

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -7,10 +7,10 @@ const RockPaperScissors = () => {
   const [computerChoice, setComputerChoice] = useState ("");
   const [iWins, setIWins] = useState(0)
   const [youWins, setYouWin] = useState(0)
-  const [youD, setYouD] = useState("")
-  const [meD, setMeD] = useState("")
+  const [youD, setYouD] = useState({})
+  const [compD, setCompD] = useState({})
 
-
+//working//
   const headerTaunt = [
     "Think you can beat me?",
     "Show me your moves!",
@@ -20,82 +20,107 @@ const RockPaperScissors = () => {
 
   const randomTaunt = headerTaunt[Math.floor(Math.random()*headerTaunt.length)];
 
+    //WHAT I WANT TO HAPPEN//
+  // set users choice on click //
+  // then get and set computer's choice//
+  // console.log both //
+  // have both display icon in cards below //
+  // compare choices to declare winner //
+  // record wins of each for score card//
+
+  const clickHandler = (click) => {
+    setChoice(click)
+    getComputerChoice()
+  }
+
   const getComputerChoice = () => {
   let computerChoices = ["rock", "paper", "scissors"];
   let randomComputerChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-  console.log(randomComputerChoice)
   setComputerChoice(randomComputerChoice)
-  }
-
-  const clickHandler = (click) => {
-  setChoice(click)
-  getComputerChoice()
-  whoWon(choice, computerChoice)
-  userDisplay(choice)
-  computerDisplay(computerChoice)
-  }
+  // computerDisplay(computerChoice)
+  }  
 
   const whoWon = (choice, computerChoice) => {
+    console.log("your choice:", choice)
+    console.log("computer's choice:", computerChoice)
   if (choice === "rock" && computerChoice === "rock")
-      alert("We'll settle it with an arm wrestle.")
+      alert("We'll settle it with an arm wrestle.  (Tie.)")
+
   if (choice === "rock" && computerChoice === "paper")
-      alert("Shhhh.  Go to sleep, LOSER!")
+      alert("Shhhh.  Go to sleep, LOSER!  (Computer wins.)")
       setIWins( iWins + 1 )
+
   if (choice === "rock" && computerChoice === "scissors")
-      alert("I will cut you!")
+      alert("I will cut you! (You win.)")
       setYouWin( youWins + 1 )
+
   if (choice === "paper" && computerChoice === "rock")
-      alert("I'll throw that rock at your head.")
+      alert("I'll throw that rock at your head.  (You win.)")
       setYouWin( youWins + 1 )
+
   if (choice === "paper" && computerChoice === "paper")
-      alert("How dare you.")
+      alert("How dare you.  (Tie.)")
+
   if (choice === "paper" && computerChoice === "scissors")
-      alert("Snip snip!")
+      alert("Snip snip!  (Computer wins.)")
       setIWins( iWins + 1 )
+
   if (choice === "scissors" && computerChoice === "rock")
-      alert("Hulk!  SMASH!")
+      alert("Hulk!  SMASH! (Computer wins.")
       setIWins( iWins + 1 )
+
   if (choice === "scissors" && computerChoice === "paper")
-      alert("You're a monster.")
+      alert("You're a monster.  (You win.)")
       setYouWin( youWins + 1 )
+
   if (choice === "scissors" && computerChoice === "scissors")
-      alert("En guarde!")
+      alert("En guarde! (Tie.)")
   }
 
-
-  const userDisplay = (choice) => {
-    if (choice === "rock")
-      return (
-        <Icon size="huge" name="hand rock" color="orange"/>
-      )
-
-    if (choice === "paper")
-      return (
-        <Icon size="huge" name="hand paper" color="purple"/>
-      )
-
-    if (choice === "scissors")
-      return (
-        <Icon size="huge" name="hand paper" color="teal"/>
-      )
+  const makeItHappen = () => {
+    whoWon(choice, computerChoice)
   }
 
-  const computerDisplay = (computerChoice) => {
-    if (choice === "rock")
-      return (
-        <Icon size="huge" name="hand rock" color="orange"/>
-      )
+  const display = [
+  {id: 1, icon: <Icon size="huge" name="hand rock" color="orange"/>},
+  {id: 2, icon: <Icon size="huge" name="hand paper" color="purple"/>},
+  {id: 3, icon: <Icon size="huge" name="hand paper" color="teal"/>}
+  ]
 
-    if (choice === "paper")
-      return (
-        <Icon size="huge" name="hand paper" color="purple"/>
-      )
+  // const userDisplay = (choice) => {
+  //   if (choice === "rock")
+  //     setYouD(display[0])
+  //     return (
+  //       {display.icon}
+  //     )
+  //   elseif(choice === "paper")
+  //     setYouD(display[1])
 
-    if (choice === "scissors")
-      return (
-        <Icon size="huge" name="hand paper" color="teal"/>
-      )
-  }
+  //   if (choice === "scissors")
+  //   setYouD(display[2])
+  // }
+
+  // const computerDisplay = (computerChoice) => {
+  //   if (choice === "rock")
+  //     return (
+  //       <Icon size="huge" name="hand rock" color="orange"/>
+  //     )
+
+  //   if (choice === "paper")
+  //     return (
+  //       <Icon size="huge" name="hand paper" color="purple"/>
+  //     )
+
+  //   if (choice === "scissors")
+  //     return (
+  //       <Icon size="huge" name="hand paper" color="teal"/>
+  //     )
+  // }
+
+  //trying to get display to work, want it to hold computer and user choices in 
+
+
+
 
 return (
   <>
@@ -120,20 +145,27 @@ return (
     >
       <Icon size="huge" name="hand scissors"/>
     </Button>
+
+    <Button color="black"
+    onClick={() => {makeItHappen()}} >
+
+      Fight me bruh
+      
+    </Button>
   </Container>
 
   <Organize>
     <Card>
       <Card.Header> You </Card.Header>
       <Card.Content>
-        {userDisplay} 
+        {/* {userDisplay}  */}
       </Card.Content>
     </Card>
 
     <Card>
     <Card.Header> Me </Card.Header>
     <Card.Content>
-        {computerDisplay}
+        {/* {computerDisplay} */}
     </Card.Content>
     </Card>
     </Organize>
---
+++ src/brainteaser.js
@@ -0,0 +1,8 @@
+array = [9, 3, 5, 6, 7, 11, 8, 1, 2, 4, 10]
+
+
+// if value of number at index 0 is greater than value of index one, switch their indexes
+// if value of number at index = is greater than value of index two, switch their indexes
+// and so on until you have checked the value at every index against the value at every index and the lower index is always lower than 
+  
+
